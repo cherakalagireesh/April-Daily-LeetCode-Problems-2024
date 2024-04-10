cf0d91a4babe0a324d97cd900080adfcce907cdf
Cherakala Gireesh <135722119+cherakalagireesh@users.noreply.github.com>
2024-04-10 20:50:45 +0530

    Time: 1 ms (99.85%), Space: 45.7 MB (72.49%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        int sum = 0;
+
+        for(int num : nums)
+        {
+            sum ^= num;
+        }   
+
+        return sum;
+    }
+}
