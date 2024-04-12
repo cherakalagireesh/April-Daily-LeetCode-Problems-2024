0a1a46e2a645d1c881f0d2e947b0026a5ffb43be
Cherakala Gireesh <135722119+cherakalagireesh@users.noreply.github.com>
2024-04-12 22:23:35 +0530

    Time: 0 ms (100.00%), Space: 40 MB (90.80%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -1,18 +1,29 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
         
-        String startBinary = Integer.toBinaryString(start);
-        String goalBinary = Integer.toBinaryString(goal);
+        // String startBinary = Integer.toBinaryString(start);
+        // String goalBinary = Integer.toBinaryString(goal);
         
-        startBinary = "0".repeat(32 - startBinary.length()).concat(startBinary);
-        goalBinary =  "0".repeat(32 - goalBinary.length()).concat(goalBinary);
+        // startBinary = "0".repeat(32 - startBinary.length()).concat(startBinary);
+        // goalBinary =  "0".repeat(32 - goalBinary.length()).concat(goalBinary);
 
+        // int count = 0;
+        
+        // for(int i = 0 ; i < 32 ; i++)
+        // {
+        //     if(startBinary.charAt(i) != goalBinary.charAt(i))
+        //         count++;
+        // }
+
+        // return count;
+
+        int num = start ^ goal;
         int count = 0;
         
-        for(int i = 0 ; i < 32 ; i++)
+        while(num != 0)
         {
-            if(startBinary.charAt(i) != goalBinary.charAt(i))
-                count++;
+            num = num & (num - 1);
+            count++;
         }
 
         return count;
